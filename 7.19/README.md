788e2c09537b534b7a2a8d5f671cc247586dc4a9
seungkyu2 <zx6658@naver.com>
2018-07-20 00:57:52 +0900

    edit readme and script

--- 7.19/README.md
+++ 7.19/README.md
@@ -4,3 +4,13 @@
 1. babel로 js변화 감지
 2. browser sync로 변화된 js파일을 읽고있는 html을 브라우저에서 변화감지
 ```
+
+## how to start
+
+```
+1. npm install
+[develop]
+2-1. npm run start
+[production]
+2-2. npm run build
+```
--- 7.19/study_project/src/index.js
+++ 7.19/study_project/src/index.js
@@ -5,7 +5,7 @@ async function cc() {
 function component() {
   var element = document.createElement("div");
 
-  element.innerHTML = "스터디재밌따@";
+  element.innerHTML = "스터디재밌aa따@";
   element.classList.add("hello");
   return element;
 }
